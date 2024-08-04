e44a11a424f70b847210e1dd963e10fd56e93c14
MyeongSeop Shin <83411963+shinmyeongsub@users.noreply.github.com>
2024-08-04 23:47:16 +0900

    feat: Entity Mapping & 연관관계 매핑

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            // close
+            em.close();
+        }
+        emf.close();
+    }
+}
